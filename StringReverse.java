dc8b88bc4396a6914c2e7c0c3290b1d57107657c
raveena25 <raveena.nagdev@ves.ac.in>
2017-12-27 13:31:30 +0530

    Add files via upload

---
+++ StringReverse.java
@@ -0,0 +1,8 @@
+public class StringReverse{
+   public static void main(String[] args) {
+      String string = "raveena";
+      String reverse = new StringBuffer(string).reverse().toString();
+      System.out.println("\nString before reverse: "+string);
+      System.out.println("String after reverse: "+reverse);
+   }
+}
